9193787bac6fec54792972f42c1b6290176d5725
Juan Carlos Coral <govindasd@gmail.com>
2018-08-24 04:23:11 -0300

    Adicionando Numeração da paginação, e deshabilitação do botão para página anterior quando estiver na primeira página e de botão para próxima página quando estiver na última página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,15 @@ export class AppComponent {
   pageSumbitted:boolean = false;
   vResultList: any[];
   vResults: any[];
+
+  // Para a paginação
+  currentSearchTerm:string;
   nextPageToken: string;
   prevPageToken: string;
-  currentSearchTerm:string;
   currentPageNum: number = 1;
   pageCoutArray: any = [1,2,3];
+  maxPageNumber: number;
+  totalSearchResults: number;
   
   videoSearchSubmit(searchValue: string, pageToken: string){
     this._searchListService.searchFor(searchValue, pageToken )
@@ -29,25 +33,34 @@ export class AppComponent {
         this.vResultList = searchResponseData.items;
         this.nextPageToken = '&pageToken=' + searchResponseData.nextPageToken;
         this.prevPageToken = '&pageToken=' + searchResponseData.prevPageToken;
-        //this.nextPageToken = searchResponseData.nextPageToken;
         this.currentSearchTerm = searchValue;
+        this.totalSearchResults = searchResponseData.pageInfo.totalResults;
       },
       error => alert(error),
       () => {
         this.searchSubmitted = true;
         if(!this.prevPageToken){
           this.pageSumbitted = false;
+          
         }
-        console.log('Page Token: ' + this.nextPageToken + ' -- PageSubmited: ' + this.pageSumbitted);
-        console.log('Current Page Number: ' + this.currentPageNum + ' -- Page Count Array: ' + this.pageCoutArray);
+        //console.log('Page Token: ' + this.nextPageToken + ' -- PageSubmited: ' + this.pageSumbitted);
+        //console.log('Current Page Number: ' + this.currentPageNum + ' -- Page Count Array: ' + this.pageCoutArray);
+
+        this.setTotalPagesCount(this.totalSearchResults);
+        console.log('Total Search Result: ' + this.totalSearchResults + ' -- Maximum page number: ' + this.maxPageNumber);
       }
     )
-  }
-  setNextPageToken(setOrUnset: boolean, pageToken){
-
   }
   resetPagination(advanceRange: number){
      this.currentPageNum += advanceRange;
      this.pageCoutArray = this.pageCoutArray.map((value)=>{return value + advanceRange});
+  }
+  setTotalPagesCount(totalSearchResults){
+    let pageNumberRemainder = totalSearchResults % 6;
+    let pageNumber = Math.floor(totalSearchResults / 6);
+    if( pageNumberRemainder > 0 )
+      this.maxPageNumber = pageNumber + 1;
+    else if(pageNumberRemainder = 0)
+      this.maxPageNumber = pageNumber;
   }
 }
